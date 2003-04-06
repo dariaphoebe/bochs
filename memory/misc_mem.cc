0c348c63d22c10558270fb002a110dc39a82634f
bdenney <bdenney>
2003-04-06 13:07:23 +0000

    - we don't need or want to register alloc_vector.  The "vector" is what   we care about, and alloc_vector only exists to guarantee that vector   can be aligned correctly within it.

--- memory/misc_mem.cc
+++ memory/misc_mem.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: misc_mem.cc,v 1.36.2.4 2003/04/04 03:46:09 slechta Exp $
+// $Id: misc_mem.cc,v 1.36.2.5 2003/04/06 13:07:23 bdenney Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -120,7 +120,7 @@ BX_MEM_C::~BX_MEM_C(void)
   void
 BX_MEM_C::init_memory(int memsize)
 {
-	BX_DEBUG(("Init $Id: misc_mem.cc,v 1.36.2.4 2003/04/04 03:46:09 slechta Exp $"));
+	BX_DEBUG(("Init $Id: misc_mem.cc,v 1.36.2.5 2003/04/06 13:07:23 bdenney Exp $"));
   // you can pass 0 if memory has been allocated already through
   // the constructor, or the desired size of memory if it hasn't
   BX_INFO(("%.2fMB", (float)(BX_MEM_THIS megabytes) ));
@@ -168,7 +168,6 @@ BX_MEM_C::register_state(bx_param_c *list_p)
   BXRS_START(BX_MEM_C, this, "", list_p, 10);
   {
     BXRS_DARRAY(vector, BX_MEM_THIS len);
-    BXRS_DARRAY(alloc_vector, BX_MEM_THIS len);
     BXRS_NUM_H(size_t, len, mem_vector_restore);
     BXRS_ENUM_D(size_t, megabytes, "(len in Megabytes)");
 #if BX_PCI_SUPPORT
