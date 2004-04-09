d61f0657d9dbdb557a8f060c09f8d28e44be9cf7
sshwarts <sshwarts>
2004-04-09 10:11:33 +0000

    extract floating point exceptions handling to another file

--- fpu/fpu_arith.cc
+++ fpu/fpu_arith.cc
@@ -27,66 +27,8 @@
 #include "bochs.h"
 #define LOG_THIS BX_CPU_THIS_PTR
 
-///////////////////////////////////////////////////////
-/// TODO: Handle FPU last instruction and data pointers 
-///////////////////////////////////////////////////////
-
-#include "softfloat-specialize.h"
 
 #if BX_SUPPORT_FPU
-
-/* Returns 1 if unmasked exception occured */
-int BX_CPU_C::FPU_exception(int exception)
-{
-  int unmasked = 0;
-
-  /* Extract only the bits which we use to set the status word */
-  exception &= (FPU_SW_Exceptions_Mask);
-
-  /* Set the corresponding exception bits */
-  FPU_PARTIAL_STATUS |= exception;
-
-  /* Set summary bits iff exception isn't masked */
-  if (FPU_PARTIAL_STATUS & ~FPU_CONTROL_WORD & FPU_CW_Exceptions_Mask)
-  {
-      FPU_PARTIAL_STATUS |= (FPU_SW_Summary | FPU_SW_Backward);
-      unmasked = 1;
-  }
-
-  if (exception & (FPU_SW_Stack_Fault | FPU_EX_Precision))
-  {
-      if (! (exception & FPU_SW_C1))
-        /* This bit distinguishes over- from underflow for a stack fault,
-             and roundup from round-down for precision loss. */
-        FPU_PARTIAL_STATUS &= ~FPU_SW_C1;
-  }
-
-  return unmasked;
-}
-
-void BX_CPU_C::FPU_stack_overflow(void)
-{
-  /* The masked response */
-  if (BX_CPU_THIS_PTR the_i387.is_IA_masked())
-  {
-      BX_CPU_THIS_PTR the_i387.FPU_push();
-      BX_WRITE_FPU_REGISTER_AND_TAG(floatx80_default_nan, FPU_Tag_Special, 0);
-  }
-  BX_CPU_THIS_PTR FPU_exception(FPU_EX_Stack_Overflow);
-}
-
-void BX_CPU_C::FPU_stack_underflow(int stnr, int pop_stack)
-{
-  /* The masked response */
-  if (BX_CPU_THIS_PTR the_i387.is_IA_masked())
-  {
-     BX_WRITE_FPU_REGISTER_AND_TAG(floatx80_default_nan, FPU_Tag_Special, stnr);
-     if (pop_stack) 
-          BX_CPU_THIS_PTR the_i387.FPU_pop();
-  }
-  BX_CPU_THIS_PTR FPU_exception(FPU_EX_Stack_Underflow);
-}
-
 softfloat_status_word_t FPU_pre_exception_handling(Bit16u control_word)
 {
   softfloat_status_word_t status;
