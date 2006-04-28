0ec8f5f83124a3ab111789002fd70859b6bf4a79
vruppert <vruppert>
2006-04-28 17:34:41 +0000

    - implemented save/restore feature in the new pit device

--- iodev/pit82c54.cc
+++ iodev/pit82c54.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: pit82c54.cc,v 1.26.2.1 2006/04/27 19:45:02 vruppert Exp $
+// $Id: pit82c54.cc,v 1.26.2.2 2006/04/28 17:34:41 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 /*
@@ -196,9 +196,29 @@ void pit_82C54::register_state(bx_param_c *parent)
 
   for (i=0; i<3; i++) {
     sprintf(name, "%d", i);
-    tim = new bx_list_c(parent, strdup(name), "");
+    tim = new bx_list_c(parent, strdup(name), "", 22);
     new bx_shadow_bool_c(tim, "GATE", "", &counter[i].GATE);
-    // TODO
+    new bx_shadow_bool_c(tim, "OUTpin", "", &counter[i].OUTpin);
+    new bx_shadow_num_c(tim, "count", "", &counter[i].count);
+    new bx_shadow_num_c(tim, "outlatch", "", &counter[i].outlatch);
+    new bx_shadow_num_c(tim, "inlatch", "", &counter[i].inlatch);
+    new bx_shadow_num_c(tim, "status_latch", "", &counter[i].status_latch);
+    new bx_shadow_num_c(tim, "rw_mode", "", &counter[i].rw_mode);
+    new bx_shadow_num_c(tim, "mode", "", &counter[i].mode);
+    new bx_shadow_bool_c(tim, "bcd_mode", "", &counter[i].bcd_mode);
+    new bx_shadow_bool_c(tim, "null_count", "", &counter[i].null_count);
+    new bx_shadow_bool_c(tim, "count_LSB_latched", "", &counter[i].count_LSB_latched);
+    new bx_shadow_bool_c(tim, "count_MSB_latched", "", &counter[i].count_MSB_latched);
+    new bx_shadow_bool_c(tim, "status_latched", "", &counter[i].status_latched);
+    new bx_shadow_num_c(tim, "count_binary", "", &counter[i].count_binary);
+    new bx_shadow_bool_c(tim, "triggerGATE", "", &counter[i].triggerGATE);
+    new bx_shadow_num_c(tim, "write_state", "", (Bit8u*)&counter[i].write_state);
+    new bx_shadow_num_c(tim, "read_state", "", (Bit8u*)&counter[i].read_state);
+    new bx_shadow_bool_c(tim, "count_written", "", &counter[i].count_written);
+    new bx_shadow_bool_c(tim, "first_pass", "", &counter[i].first_pass);
+    new bx_shadow_bool_c(tim, "state_bit_1", "", &counter[i].state_bit_1);
+    new bx_shadow_bool_c(tim, "state_bit_2", "", &counter[i].state_bit_2);
+    new bx_shadow_num_c(tim, "next_change_time", "", &counter[i].next_change_time);
   }
 }
 #endif
